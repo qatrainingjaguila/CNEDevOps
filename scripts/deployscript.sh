728f3af2c1475eac1b0ac7e2dc22a2e14c83d11a
qatrainingjaguila <jaguila@academytrainee.com>
2020-11-19 21:57:44 +0000

    k8s test

--- scripts/deployscript.sh
+++ scripts/deployscript.sh
@@ -3,12 +3,12 @@
 echo "calling deployscript. Ensure project namespace and secret.yaml exist"
 ls
 kubectl get namespace
-sudo mv secret.yaml /K8s
 #kubectl apply
 cd K8S
 ls
 cd ..
 cp secret.yaml Project/K8S
+cd K8S
 kubectl apply -f nginx-conf.yaml -n project
 kubectl apply -f secret.yaml -n project
 kubectl apply -f backend-app.yaml -f backend-cip.yaml -f flask-app.yaml -f flask-cip.yaml -f nginx.yaml -f nginx-lb.yaml
