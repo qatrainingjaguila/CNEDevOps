daa2e4e36e3cbdded802397faf917345f3b8dcd0
qatrainingjaguila <jaguila@academytrainee.com>
2020-11-19 21:33:30 +0000

    test kubectl

--- scripts/deployscript.sh
+++ scripts/deployscript.sh
@@ -2,5 +2,9 @@
 
 echo "calling deployscript. Ensure project namespace and secret.yaml exist"
 ls
-kubectl get ns
+kubectl get namespace
+mv secret.yaml /K8s
 #kubectl apply
+kubectl apply -f nginx-conf.yaml -n project
+kubectl apply -f secret.yaml -n project
+kubectl apply -f backend-app.yaml -f backend-cip.yaml -f flask-app.yaml -f flask-cip.yaml -f nginx.yaml -f nginx-lb.yaml
