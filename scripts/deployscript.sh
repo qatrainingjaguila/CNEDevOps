3dd8bd751ed263c22dbbe32ecf55506243b077ac
qatrainingjaguila <jaguila@academytrainee.com>
2020-11-19 21:34:42 +0000

    test kubectl

--- scripts/deployscript.sh
+++ scripts/deployscript.sh
@@ -5,6 +5,8 @@ ls
 kubectl get namespace
 mv secret.yaml /K8s
 #kubectl apply
+cd K8S
+ls
 kubectl apply -f nginx-conf.yaml -n project
 kubectl apply -f secret.yaml -n project
 kubectl apply -f backend-app.yaml -f backend-cip.yaml -f flask-app.yaml -f flask-cip.yaml -f nginx.yaml -f nginx-lb.yaml
