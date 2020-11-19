145ec586a3209204d9d5399a6d6486f01db56c80
qatrainingjaguila <jaguila@academytrainee.com>
2020-11-19 19:49:30 +0000

    compose typo 2

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,8 +7,8 @@ cd CNEDevOps
 docker-compose up -d
 #docker exec run pytest
 echo "Running tests. Ensure Database URIs have been set"
-sudo docker exec -frontend bash -c "pytest tests/ --cov application"
-sudo docker exec -backend bash -c "pytest tests/ --cov application"
+sudo docker exec frontend bash -c "pytest tests/ --cov application"
+sudo docker exec backend bash -c "pytest tests/ --cov application"
 #clean up
 cd ..
 rm -rf CNEDevOps
