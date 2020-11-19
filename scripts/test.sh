f6b56c21f263944b6fbd77107830733c6e8a61f2
qatrainingjaguila <jaguila@academytrainee.com>
2020-11-19 19:31:54 +0000

    jenkins test 5

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 #clone repo
-echo "hello from test"
+echo "Cloning"
+git clone --branch develop https://github.com/qatrainingjaguila/CNEDevOps.git
+cd CNEDevOps
+#run app
+sudo docker-compose up -d
 #docker exec run pytest
-
-#docker exec run pytest
-
+echo "Running tests. Ensure Database URIs have been set"
+sudo docker exec -frontend bash -c "pytest tests/ --cov application"
+sudo docker exec -backend bash -c "pytest tests/ --cov application"
+#clean up
+cd ..
+rm -rf CNEDevOps
 #exit
