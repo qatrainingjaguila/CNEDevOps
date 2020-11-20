9b9eae38a1aa19d2cc1fc4e4d0f41439156bca30
qatrainingjaguila <jaguila@academytrainee.com>
2020-11-20 12:16:40 +0000

    EKS

--- Terraform/EKS/variables.tf
+++ Terraform/EKS/variables.tf
@@ -3,5 +3,29 @@ variable "subnet_ids"{
 }
 
 variable "role_arn"{
+    default = "arn:aws:iam::939621893623:role/ClusterRole"
+}
 
+variable "EKS_security_group_ids"{
+
+}
+
+variable "node_role_arn"{
+    default = "arn:aws:iam::939621893623:role/NodeInstanceRole"
+}
+
+variable "nodegroup_subnet_ids" {
+
+}
+
+variable "ami_type" {
+    default = "AL2_x86_64"
+}
+
+variable "instance_types" {
+    default = ["t3.micro"]
+}
+
+variable "remote_SG_ids" {
+    
 }
