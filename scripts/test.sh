f830393421e397084a70c1ccebfe05a232d91622
qatrainingjaguila <jaguila@academytrainee.com>
2020-11-20 14:31:21 +0000

    test push

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,8 +5,7 @@ git clone --branch main https://github.com/qatrainingjaguila/CNEDevOps.git
 cd CNEDevOps
 #run app
 echo ${DATABASE_URI}
-sudo docker-compose build
-sudo docker-compose up -d -E DATABASE_URI=${DATABASE_URI} -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -E SECRET_KEY=${SECRET_KEY}
+sudo docker-compose up --build -d -E DATABASE_URI=${DATABASE_URI} -E TEST_DATABASE_URI=${TEST_DATABASE_URI} -E SECRET_KEY=${SECRET_KEY}
 #docker exec run pytest
 echo "Running tests. Ensure Database URIs have been set"
 sudo docker exec frontend bash -c "pytest tests/ --cov application"
