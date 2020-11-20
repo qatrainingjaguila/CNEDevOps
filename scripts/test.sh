0883b0100bee3083103e0e95d02e7a6beeade423
qatrainingjaguila <jaguila@academytrainee.com>
2020-11-20 14:13:43 +0000

    test push

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,7 @@ echo "Cloning"
 git clone --branch main https://github.com/qatrainingjaguila/CNEDevOps.git
 cd CNEDevOps
 #run app
-echo {DATABASE_URI}
+echo ${DATABASE_URI}
 sudo docker-compose up -d
 #docker exec run pytest
 echo "Running tests. Ensure Database URIs have been set"
