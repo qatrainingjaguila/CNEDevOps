2f72185a62c475a9a955e647a277528550e964e1
qatrainingjaguila <jaguila@academytrainee.com>
2020-11-22 22:12:47 +0000

    rollout

--- scripts/deployscript.sh
+++ scripts/deployscript.sh
@@ -3,8 +3,8 @@
 echo "calling deployscript. Ensure project namespace and secret.yaml exist in K8S"
 cd K8S
 # Running kubernetes deployment commands
-kubectl delete deployment flask-app -n project
-kubectl delete deployment backend -n project
+kubectl rollout restart deployment/flask-app -n project
+kubectl rollout restart deployment/backend -n project
 kubectl apply -f nginx-conf.yaml -n project
 kubectl apply -f backend-app.yaml -f backend-cip.yaml -f flask-app.yaml -f flask-cip.yaml -f nginx.yaml -f nginx-lb.yaml
 #retrieving loadbalancer information
